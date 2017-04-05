fd0b8bc17e485d6045b93e9a538136ccf0eabb00
Artem Arshaniy <artem.arshaniy108@gmail.com>
2017-04-05 15:00:45 +0300

    commit

---
+++ javascript/js/script.js
@@ -0,0 +1,64 @@
+
+var content = document.getElementById("content");
+var btn = document.getElementById('btn');
+var btnSrch = document.getElementById('btnSrch');
+
+window.onload = function() {
+
+
+ var request = new XMLHttpRequest();
+
+ request.open('GET', 'data.json');
+ request.onload = function () {
+     var ourData = JSON.parse(request.responseText);
+    renderHTML(ourData);
+ };
+ request.send();
+
+ function  renderHTML(data) {
+   var htmlString = "";
+    for (i = 0; i < data.length; i++) {
+     htmlString += "<p>" + "NAME:  " + data[i].first_name + "  " + data[i].last_name + " EMAIL:  " + data[i].email + "</p>";
+   }
+     content.insertAdjacentHTML('beforeend', htmlString);
+ };
+
+ btnSrch.addEventListener("click", function (data) {
+
+    var valueSrch = document.getElementById('valueSrch').value;
+
+    for (var i = 0; i < data.length; i++) {
+      if ("Sandra" == valueSrch) {
+        console.log(data[3].first_name);
+      } else {
+        console.log("not found");
+      }
+
+    }
+
+
+   });
+
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {}
